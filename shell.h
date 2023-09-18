ffc49dcb797d8c8d00193ee84afd48c3d3f78c90
joejoturkson89 <turksonjoejo@gmail.com>
2023-09-18 14:43:28 -0700

    updated

--- shell.h
+++ shell.h
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-
+#include <sys/stat.h>
 
 void mj_print(const char *comment);
 
@@ -15,6 +15,14 @@ void mj_display_prompt(void);
 void mj_exec_command(const char *command);
 
 void input_command(char *command, size_t size);
+
+char mj_path(ino_t *info, char *pathstr, char *command);
+
+
+int is_command(ino_t *info, char *path);
+
+char* mj_dup_chars(const char* pathstr, int start, int stop);
+
 
 
 #endif
